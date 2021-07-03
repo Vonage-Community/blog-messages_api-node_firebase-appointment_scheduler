70c2abd73ea1d0ec80190615d9872a8a1b8d4621
Amanda Cavallaro <amanda.cavallaro@vonage.com>
2021-07-03 17:12:33 +0100

    Adds Database and cancel functionalities

--- script/server.js
+++ script/server.js
@@ -6,16 +6,18 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const Vonage = require("@vonage/server-sdk");
 
+// TODO remove db being used
 console.log(`USING DB: ${process.env.FIREBASE_DATABASE_URL}`);
 
 var serviceAccount = require("../serviceAccountKey.json");
 
+// Initializes firebase
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: `${process.env.FIREBASE_DATABASE_URL}`,
 });
 
-ref = admin.database().ref();
+ref = admin.database().ref("/myAppointments");
 
 // Sends SMS with Vonage API
 const vonage = new Vonage({
@@ -27,25 +29,28 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const getDateTime = (slot) => {
+  return slot.split("T");
+};
+
 app.post("/appointment", async (request, response) => {
+  // TODO use this phonenumber
   let phonenumber = request.body.phonenumber;
   let slot = request.body.slotdate;
+  let [date, time] = getDateTime(slot);
 
-  // TODO Function to check the slots taken from the database
-  // and initialise the datepicker
   checkIfAvailable = async (slot) => {
-    // TODO Check if date is available from Firebase and adds invalid date on the datepicker
     let snapshot = await ref.orderByChild("date").once("value");
 
     let available = true;
     snapshot.forEach((data) => {
       let dataval = data.val();
-      // console.log(`DATAVAL`, dataval);
+      console.log(`DATAVAL`, dataval);
       for (let key in dataval) {
         let datapoint = dataval[key];
-        // console.log(`DATA IS`, datapoint);
-        console.log(`COMPARING`, slot, datapoint.date);
-        if (slot === datapoint.date) {
+        console.log(`DATA IS`, datapoint);
+        console.log(`COMPARING`, slot, datapoint);
+        if (slot === datapoint) {
           available = false;
         }
       }
@@ -53,31 +58,42 @@ app.post("/appointment", async (request, response) => {
     return available;
   };
 
-  // Sends an SMS back to the user's phone using the Vonage SMS API
-  sendSMStoUser = async (slot) => {
-    // TODO time and date should be retrieved from the input datepicker
-    // Format is 2021-06-01T08:30
-    // let dateRe = /.\T(.*)/;
-    // let myDate = dateRe.exec(slot);
-    // let timeRe = /^(.*?)T/;
-    // let myTime = timeRe.exec(slot);
-    // time = myTime;
-    // date = myDate;
-    time = "08:30";
-    date = "2021-06-01";
-    // Generates an id https://gist.github.com/gordonbrander/2230317
-    let randomIdGenerator = "_" + Math.random().toString(36).substr(2, 9);
+  // example in firebase docs:
+  //   const usersRef = ref.child('users');
+  // usersRef.child('alanisawesome').set({
+  //   date_of_birth: 'June 23, 1912',
+  //   full_name: 'Alan Turing'
+  // });
+  // usersRef.child('gracehop').set({
+  //   date_of_birth: 'December 9, 1906',
+  //   full_name: 'Grace Hopper'
+  // });
 
+  // TODO Confirm - Persists a slot to Firebase
+  addToDatabase = () => {
+    let code = "_" + Math.random().toString(36).substr(2, 9);
+
+    ref.child(code).set({
+      date: slot,
+      userId: code,
+    });
+
+    return code;
+  };
+
+  // Sends an SMS back to the user's phone using the Vonage SMS API
+  sendSMStoUser = async (code) => {
     const from = process.env.VONAGE_FROM_NUMBER;
     // TODO the below to has to be the user's phonenumber
     const to = process.env.VONAGE_TO_NUMBER;
-    const text = `Meeting booked at ${time} on date: ${date}. Please save this code in case you'd like to cancel your appointment: ${randomIdGenerator}`;
+    const text = `Meeting booked at ${time} on date: ${date}. Please save this code in case you'd like to cancel your appointment: ${code}`;
+    // console.log(`TEXTING to ${to} from ${from}: ${text}`);
     const result = await new Promise((resolve, reject) => {
       vonage.message.sendSms(from, to, text, (err, responseData) => {
         if (err) {
           return reject(new Error(err));
         } else {
-          if (responseData.messages[0]["status"] === "0") {
+          if (responseData.messages[0].status === "0") {
             return resolve(
               `Message sent successfully: ${responseData.messages[0]["message-id"]}`
             );
@@ -95,31 +111,30 @@ app.post("/appointment", async (request, response) => {
 
   let available = await checkIfAvailable(slot);
   if (available) {
-    console.log("DB IS NOW TAKEN");
-    sendSMStoUser();
-    response.send(`SLOT IS AVAILABLE: ${slot}`);
+    console.log("SLOT WAS AVAILABLE, BOOKING IT NOW");
+    let code = addToDatabase();
+    await sendSMStoUser(code);
+    response.send(`SLOT IS AVAILABLE, BOOKING IT NOW: ${slot}`);
   } else {
     // send user error
     response.send(
-      `SORRY, NEED TO CHOOSE A DIFFERENT SLOT.${slot} iS ALREADY BUSY`
+      `SORRY, NEED TO CHOOSE A DIFFERENT SLOT.${slot} IS ALREADY BUSY`
     );
   }
+});
+
+app.post("/cancelAppointment", async (request, response) => {
+  let code = request.body.code;
+  console.log("SLOT REMOVED");
+
+  removeSlotFromDB = (code) => {
+    ref.child(code).remove();
+  };
+  removeSlotFromDB(code);
+
+  response.send(`You've been removed`);
 });
 
 app.listen(port, () => {
   console.log(`I run on port ${port}`);
 });
-
-// TODO Confirm - Persists a slot to Firebase
-// exports.sendSMS = functions.database.ref("/myAppointments").push({
-//   date: date,
-//   time: time,
-//   userId: "123",
-// });
-
-// TODO Cancel - Remove information from the Database,
-// check the syntax for deleting
-// Source: https://firebase.google.com/docs/database/web/read-and-write#web-v8_7
-// exports.removeSlot = (code) => {
-//   exports.removeSlot = functions.database.ref.child("new_slot" + code).remove();
-// };
